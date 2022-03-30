d871a168428ea52caa7e357f55987c9fe4d6627d
Uğur DURA <47089334+UgurDURA@users.noreply.github.com>
2022-03-31 02:35:10 +0300

    question 3 [done]

--- PythonExercise2.py
+++ PythonExercise2.py
@@ -59,9 +59,24 @@ print(matrix)
 print(matrix.shape)
 
 
+# -------------------------------------------------------
+# QUESTION2 
+
 first_col = matrix[:,0]
 last_col = matrix [:,np.size(matrix,1)-1]
 
 print(first_col)
 print(last_col)
 
+plt.scatter(last_col, first_col)
+
+
+y_mean = np.mean(first_col)
+
+plt.plot([0,14], [y_mean, y_mean])
+plt.hlines(y_mean, xmin=0, xmax=14)
+
+
+
+
+plt.show()
