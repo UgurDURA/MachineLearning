8ed0a672845b0a60155f6f1891aedd4f5535828d
Uğur DURA <47089334+UgurDURA@users.noreply.github.com>
2022-04-22 07:32:29 +0300

    coefficient calculator implemented for residual error

--- CE475-LAB3/lab3.py
+++ CE475-LAB3/lab3.py
@@ -1,5 +1,6 @@
 import csv
 from re import X
+from matplotlib import pyplot as plt
 import numpy as np 
 
 x1_AgeList =np.array([])
@@ -23,6 +24,27 @@ print(x1_AgeList)
 print(x2_HeightList)
 print(x3_MentalStrengthList)
 print(x4_SkillList)
+
+combined_matrix = np.column_stack((np.ones(len(x1_AgeList)), x1_AgeList, x2_HeightList, x3_MentalStrengthList, x4_SkillList))
+np.set_printoptions(precision=4)
+
+print(combined_matrix)
+
+# define a function to find residual error and y predictions
+def calculator_coef(matrix, out):
+    matrix_t = matrix.transpose()
+
+    coef = np.dot(np.dot(np.linalg.inv(np.dot(matrix_t, matrix)), matrix_t), out)
+    y_prediction = np.dot(matrix, coef)
+    error = np.abs(out - y_prediction)
+
+    plt.scatter(y_prediction, error)
+    plt.title("Residual Error Plot")
+    plt.xlabel("Predictions")
+    plt.ylabel("Error")
+
+    return y_prediction, plt
+
 
 
 
