c7d926fc12c04b330bda6c5d19622a96cc1142d9
Uğur DURA <47089334+UgurDURA@users.noreply.github.com>
2022-06-11 04:44:44 +0300

    R2 fixed

--- CE475_Project.py
+++ CE475_Project.py
@@ -1,10 +1,12 @@
 def warn(*args, **kwargs):
     pass
+from cProfile import label
 from unittest import result
 import warnings
 from sklearn import linear_model
 
 from sklearn.preprocessing import PolynomialFeatures
+from sklearn.svm import SVC
 warnings.warn = warn
 import csv
 from nis import match
@@ -28,7 +30,7 @@ from sklearn.model_selection import cross_val_score, train_test_split
 from sklearn.linear_model import  LinearRegression, Ridge, RidgeCV, Lasso, LassoCV
 np.warnings.filterwarnings('ignore', category=np.VisibleDeprecationWarning)   
 from statsmodels.stats.outliers_influence import variance_inflation_factor
-
+from sklearn.linear_model import LinearRegression
 
 
 
@@ -127,4 +129,147 @@ pandasDF.__delitem__("X6")
 X_Matrix = np.delete(X_combined_matrix, 5, 1)
 
 print(pandasDF)
-print(X_Matrix)
+
+############################################################################################################################################
+                                            #MTrain Test Split  
+############################################################################################################################################
+
+X_train = np.array([])
+X_test = np.array([])
+Y_train = np.array([]) 
+Y_test = np.array([])
+
+def SplitData(X_Matrix, Y, testSize):
+    X_train, X_test, Y_train, Y_test= train_test_split(X_Matrix,Y, test_size=testSize, shuffle=False)
+    return X_train, X_test, Y_train, Y_test
+   
+
+ ############################################################################################################################################
+                                            #Selection of Accuracy and Error Metrics
+############################################################################################################################################
+
+def calculator_error(y_actual, y_pred, metric):
+    rss, tss = 0, 0
+
+    rss = sum((y_actual - y_pred) ** 2)
+    tss = sum((y_actual - np.mean(y_actual)) ** 2)
+
+    r_square = 1 - (rss / tss)
+    MAE = np.mean(sum(np.abs(y_actual - y_pred)))
+    MSE = np.mean(sum((y_actual - y_pred) ** 2))
+    RMSE = np.sqrt(MSE)
+
+    if metric == "RSquare":
+        return r_square
+    elif metric == "MSE":
+        return MSE
+    elif metric == "MAE":
+        return MAE
+    elif metric == "RMSE":
+        return RMSE
+    else:
+        return 0
+
+############################################################################################################################################
+                                            #Cross Validation  
+############################################################################################################################################
+from sklearn.model_selection import KFold
+
+def get_score(model, X_train, X_test, Y_train, Y_test):
+    model.fit(X_train, Y_train)
+    return model.score(X_test, Y_test)
+
+ 
+
+def Kfold_CrossValidation_Model_R2(model, X_Matrix, Y, cv):
+    CV_results = np.array([])
+    kf = KFold(n_splits=cv)
+    
+    for train_index, test_index in kf.split(X_Matrix, Y):
+        X_train, X_test, Y_train, Y_test = X_Matrix[train_index], X_Matrix[test_index],Y[train_index], Y[test_index]
+        CV_results = np.append(CV_results, get_score(model, X_train, X_test, Y_train, Y_test))
+        print(CV_results)
+    return CV_results
+
+
+
+############################################################################################################################################
+                                            #Multiple Linear Regression with Different Train-Test Split 
+############################################################################################################################################
+
+
+def mullin_coef(X, y):
+    # Calculating coefficients using the linear algebra equation:
+    B_hat = np.dot(X.T, X)
+    B_hat = np.linalg.inv(B_hat)
+    B_hat = np.dot(B_hat, X.T)
+    B_hat = np.dot(B_hat, y)
+
+    return B_hat
+
+
+coefficients = mullin_coef(X_Matrix, Y)
+Y_predictions_MultiLinearRegression = np.dot(X_Matrix, coefficients)
+
+mse = np.array([], dtype=float64)
+r_square = np.array([])
+mae = np.array([])
+rmse = np.array([])
+rss = np.array([])
+
+for i in range(1,10):
+    train_data, test_data, train_Y, test_Y = SplitData(X_Matrix, Y, 5 * i)
+    coefficients = mullin_coef(train_data, train_Y)
+    Y_predictions_MultiLinearRegression = np.dot(test_data, coefficients)
+    Y_predictions_RSquare = np.dot(X_Matrix,coefficients) #To check the model fit onto the Dataset
+    r_error= calculator_error(Y, Y_predictions_RSquare, "RSquare")
+    RSS = sum((Y - Y_predictions_RSquare) ** 2)
+    MSE = calculator_error(test_Y, Y_predictions_MultiLinearRegression, "MSE")
+    MAE = calculator_error(test_Y, Y_predictions_MultiLinearRegression, "MAE")
+    RMSE = calculator_error(test_Y, Y_predictions_MultiLinearRegression, "RMSE")
+    rmse = np.append(rmse, RMSE)
+    mse = np.append(mse, MSE)
+    mae = np.append(mae, MAE)
+    rss = np.append(rss, RSS)
+    r_square = np.append(r_square, r_error)
+    print("For k = ", i*5)
+    print("R^2 = ", r_error)
+    print("MAE = ", MAE)
+    print("MSE = ", MSE)
+    print("RMSE = ", RMSE)
+    print()
+
+range = np.linspace(1,50,num=9)
+fig,ax=plt.subplots(figsize=(6,6))
+ax.plot(range, rss)
+ax.set_xlabel('Test Size (%)',fontsize=20)
+ax.set_ylabel('RSS Calculation',fontsize=20)
+ax.set_title('Train/Test Split vs RSS ',fontsize=25)
+plt.show()
+
+ 
+
+        
+
+
+
+kFoldValues = [5, 10, 15, 20]
+# for i in kFoldValues:
+#     CV_results = np.append(CV_results,MultiLinear_Kfold_CrossValidation(X_Matrix, Y, i, 15) ) 
+    
+
+
+
+for i in kFoldValues:
+    Multilinear_CV = Kfold_CrossValidation_Model_R2(LinearRegression(),X_Matrix,Y,10)
+    
+
+
+print(Multilinear_CV)
+
+
+a = cross_val_score(RandomForestRegressor(),X_Matrix, Y)
+
+
+print("---------........=========================+>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Cross Validation with A")
+print(a)
